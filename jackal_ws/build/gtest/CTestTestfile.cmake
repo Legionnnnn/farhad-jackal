bf1164c2dfed8965c0e0cbd635bc2f1effa5cae1
MFM <162095614+Legionnnnn@users.noreply.github.com>
2025-09-24 18:09:54 +0100

    Delete jackal_ws/build directory

--- jackal_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/legion/projects/jackal-project/jackal_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
